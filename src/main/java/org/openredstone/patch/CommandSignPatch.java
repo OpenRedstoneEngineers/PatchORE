301a79a09a5b31e80d219761e0dbc91c54594a33
ayunami2000 <spwilliamsiam@gmail.com>
2022-06-22 17:53:48 -0400

    Massive rewrite of my patch

--- src/main/java/org/openredstone/patch/CommandSignPatch.java
+++ src/main/java/org/openredstone/patch/CommandSignPatch.java
@@ -1,13 +1,22 @@
 package org.openredstone.patch;
 
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.item.ItemStack;
+import com.google.gson.JsonSyntaxException;
+import de.tr7zw.nbtapi.NBTCompound;
+import de.tr7zw.nbtapi.NBTItem;
+import de.tr7zw.nbtapi.NBTType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.chat.ComponentSerializer;
+import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack;
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandSignPatch extends Patch implements Listener {
@@ -22,18 +31,54 @@ public class CommandSignPatch extends Patch implements Listener {
 
         if (!Tag.SIGNS.getValues().contains(bl.getType())) return;
 
-        ItemStack sign = CraftItemStack.asNMSCopy(event.getItemInHand());
+        NBTItem sign = new NBTItem(event.getItemInHand());
 
-        if (!sign.hasTag()) return;
+        if (!sign.hasNBTData()) return;
 
-        NBTTagCompound tag = sign.getTag().getCompound("BlockEntityTag");
+        if (!(sign.hasKey("BlockEntityTag") && sign.getType("BlockEntityTag").equals(NBTType.NBTTagCompound))) return;
 
-        if (tag.getString("Text1").contains("run_command")
-                || tag.getString("Text2").contains("run_command")
-                || tag.getString("Text3").contains("run_command")
-                || tag.getString("Text4").contains("run_command")) {
-            sendMessage(event.getPlayer(), "Yeah, no command signs.");
-            event.setCancelled(true);
+        NBTCompound tag = sign.getCompound("BlockEntityTag");
+
+        boolean block = false;
+
+        Player player = event.getPlayer();
+
+        if (tag.hasKey("Text1") && tag.getType("Text1").equals(NBTType.NBTTagString)) block = isLineUnsafe(tag.getString("Text1"), player);
+        if (!block && tag.hasKey("Text2") && tag.getType("Text2").equals(NBTType.NBTTagString)) block = isLineUnsafe(tag.getString("Text2"), player);
+        if (!block && tag.hasKey("Text3") && tag.getType("Text3").equals(NBTType.NBTTagString)) block = isLineUnsafe(tag.getString("Text3"), player);
+        if (!block && tag.hasKey("Text4") && tag.getType("Text4").equals(NBTType.NBTTagString)) block = isLineUnsafe(tag.getString("Text4"), player);
+
+        if (block) event.setCancelled(true);
+    }
+
+    private boolean isLineUnsafe(String text, Player player) {
+        BaseComponent[] components;
+        try {
+            components = ComponentSerializer.parse(text);
+        } catch (Exception e) {
+            sendMessage(player, "Yeah, no.");
+            return true;
+        }
+
+        for (BaseComponent component : components) {
+            if (component.getClickEvent() != null && component.getClickEvent().getAction() == ClickEvent.Action.RUN_COMMAND) {
+                sendMessage(player, "Yeah, no command signs.");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @EventHandler
+    public void onServerCommand(ServerCommandEvent event) {
+        CommandSender commandSender = event.getSender();
+
+        if (commandSender instanceof BlockCommandSender) {
+            Material type = ((BlockCommandSender) commandSender).getBlock().getType();
+
+            if (!(type == Material.COMMAND_BLOCK || type == Material.CHAIN_COMMAND_BLOCK || type == Material.REPEATING_COMMAND_BLOCK))
+                event.setCancelled(true);
         }
     }
 }
